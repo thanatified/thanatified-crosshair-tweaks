b168de73589258c07728cddbcb3e82233d74b93c
thanatified <dquirk145@gmail.com>
2026-08-17 06:27:15 +0100

    Create ModMenuIntegration.java

---
+++ src/main/java/com/crosshairtweaks/config/ModMenuIntegration.java
@@ -0,0 +1,11 @@
+package com.crosshairtweaks.config;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> CrosshairConfigScreen.create(parent);
+    }
+}
