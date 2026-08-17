8198af5ce0f6bafd9aa997223fb33214df300b53
thanatified <dquirk145@gmail.com>
2026-08-17 04:42:16 +0100

    Update CrosshairTweaksClient.java

--- src/main/java/com/crosshairtweaks/CrosshairTweaksClient.java
+++ src/main/java/com/crosshairtweaks/CrosshairTweaksClient.java
@@ -7,36 +7,36 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.option.KeyBinding.Category;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class CrosshairTweaksClient implements ClientModInitializer {
 
-	public static CrosshairConfig CONFIG;
+    public static CrosshairConfig CONFIG;
 
-	private static KeyBinding openConfigKey;
+    private static KeyBinding openConfigKey;
 
-	@Override
-	public void onInitializeClient() {
-		CONFIG = CrosshairConfig.load();
+    @Override
+    public void onInitializeClient() {
+        CONFIG = CrosshairConfig.load();
 
-		openConfigKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-				"key.crosshairtweaks.open_config",
-				InputUtil.Type.KEYSYM,
-				GLFW.GLFW_KEY_COMMA,
-				"key.category.crosshairtweaks"
-		));
+        openConfigKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.crosshairtweaks.open_config",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_COMMA,
+                Category.create("key.category.crosshairtweaks")
+        ));
 
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (openConfigKey.wasPressed()) {
-				if (client.currentScreen == null) {
-					client.setScreen(new CrosshairConfigScreen(null, CONFIG));
-				}
-			}
-		});
-	}
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openConfigKey.wasPressed()) {
+                if (client.currentScreen == null) {
+                    client.setScreen(new CrosshairConfigScreen(null, CONFIG));
+                }
+            }
+        });
+    }
 
-	public static MinecraftClient client() {
-		return MinecraftClient.getInstance();
-	}
-}
+    public static MinecraftClient client() {
+        return MinecraftClient.getInstance();
+    }
